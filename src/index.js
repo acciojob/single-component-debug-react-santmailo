39228e6f2909ddf9646d20b5018392d891bbe6ee
santmailo <iamsunil006@gmail.com>
2024-08-22 17:48:31 +0530

    solved attempt 1

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </React.StrictMode>,
+  document.getElementById("root")
+);
